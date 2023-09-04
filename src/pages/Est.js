35ae1f6c1a920b1b450649f5051e69618c7458e2
Jesica Rodriguez <jeesssik@gmail.com>
2023-09-04 15:47:45 -0300

    Update Est.js

--- src/pages/Est.js
+++ src/pages/Est.js
@@ -15,14 +15,22 @@ const Estadisticas = () => {
     return parseInt(current.edad) < parseInt(prev.edad) ? current : prev;
   });
 
+  // Si hay más de uno con la misma edad que la de mayor edad
+  const igualesMayorEdad= data.filter((persona)=>parseInt(personaMayorEdad.edad)==parseInt(persona.edad))
+
   return (
     <div>
       <h1>Estadísticas</h1>
       <p>Cantidad de personas mayores de 35 años: {mayoresDe35.length}</p>
       <p>
         Persona de mayor edad:{' '}
-        {personaMayorEdad.nombre} ({personaMayorEdad.edad} años)
+        {personaMayorEdad.nombre} ({personaMayorEdad.edad} años y son {igualesMayorEdad.length} personas las que tienen la misma edad)
       </p>
+      {igualesMayorEdad.map((pos)=>{
+        return(
+          <p>{pos.nombre}</p>
+        )
+      })}
       <p>
         Persona de menor edad:{' '}
         {personaMenorEdad.nombre} ({personaMenorEdad.edad} años)
@@ -32,3 +40,4 @@ const Estadisticas = () => {
 };
 
 export default Estadisticas;
+
